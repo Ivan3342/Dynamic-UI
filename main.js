ff1960d2589d14d5e13e477d148978ff69fa0625
Ivan Stojadinovic <stojadinovicivan0@gmail.com>
2025-07-25 10:32:31 +0200

    fixed the dropdown menu

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
 const dropdown1 = document.querySelector("#dropdown1");
 document.querySelector("#dropdown1li").addEventListener("mouseover", (e) => {
-    dropdown1.classList.toggle("visible");
+    dropdown1.classList.remove("visible");
+})
+
+document.querySelector("#dropdown1li").addEventListener("mouseleave", (e) => {
+    dropdown1.classList.add("visible");
+})
+
+dropdown1.addEventListener("mouseover", () => {
+    dropdown1.classList.remove("visible");
+})
+
+dropdown1.addEventListener("mouseleave", () => {
+    dropdown1.classList.add("visible");
 })
