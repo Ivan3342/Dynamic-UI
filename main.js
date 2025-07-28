67fc8cb4d11059e5b6bc2ec489458f64a3c3d9c8
Ivan Stojadinovic <stojadinovicivan0@gmail.com>
2025-07-28 13:06:54 +0200

    styling complete

--- main.js
+++ main.js
@@ -23,19 +23,8 @@ const images = [
     "images/Gemini_Generated_Image_pm5g10pm5g10pm5g.png"
 ]
 
-const changeImage = () => {
-    const imageElement = document.querySelector("#imgElement");
-    const allImagesDivs = document.querySelectorAll("img");
-    id = 0;
-    setInterval(() => {
-        for(let i = 0; i < images.length; i++) {
-            allImagesDivs[0].src = images[0+i];
-            allImagesDivs[1].src = images[1+i];
-            allImagesDivs[2].src = images[2+i];
-            allImagesDivs[3].src = images[3+i];
-            allImagesDivs[4].src = images[4+i];
-        }
-    }, 2000)
-}
+const ImageElement = () => {
+    let id = 0;
+    const imageElement = document.querySelector("#imageElement");
 
-changeImage();
+}
